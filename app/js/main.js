0388e8f20ad343afd7fc469a2a105ec9c3ca740f
DimaMelnichuk <extezzy1995@gmail.com>
2020-03-18 12:51:17 +0300

    Add block offer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function(){
  
- 	/***************** Name ********************/
+	 /***************** Slick ********************/
+	 $('.offer-slider').slick({
+		arrows:false,
+		asNavFor: ".offer-slider__dots",
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+  		autoplaySpeed: 4000,
+	 });
 
+	 $('.offer-slider__dots').slick({
+		arrows:false,
+		asNavFor: ".offer-slider",
+		slidesToShow: 5,
+		slidesToScroll: 1,
+	 });
 });
